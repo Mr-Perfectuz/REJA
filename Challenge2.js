85b1e4ed954ad85c207fcb43155d345f5b2a99b0
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2023-09-16 09:35:35 +0900

    CHallenge has been solved

---
+++ Challenge2.js
@@ -0,0 +1,46 @@
+const animal_list = [
+  "fox",
+  "ant",
+  "bird",
+  "lion",
+  "wolf",
+  "deer",
+  "beer",
+  "frog",
+  "hen",
+  "mole",
+  "duck",
+  "goat",
+  "dog",
+  "cat",
+  "bat",
+  "cock",
+  "cow",
+];
+
+function findAnimal(txt) {
+  const lowercaseTxt = txt.toLowerCase();
+
+  const result = [];
+
+  for (const animal of animal_list) {
+    const lowercaseAnimal = animal.toLowerCase();
+
+    if (check_Each_Letters(lowercaseAnimal, lowercaseTxt)) {
+      result.push(animal);
+    }
+  }
+  return result;
+}
+
+function check_Each_Letters(listdagi_soz, input_soz) {
+  for (const harf of listdagi_soz) {
+    if (!input_soz.includes(harf)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const find_animal = findAnimal("fdgwocalt");
+console.log(find_animal);
