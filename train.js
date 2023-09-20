980c2d1bbbc67d9440017674c90aa11b3d62c702
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2023-09-21 08:47:51 +0900

    D-Task has been finished

--- train.js
+++ train.js
@@ -28,7 +28,54 @@ function checkSimilarity(string1, string2) {
 }
 
 var result1 = checkSimilarity("laepp", "apple");
-console.log(result1);
+// console.log(result1);
 
 var result2 = checkSimilarity("listen", "silent");
-console.log(result2);
+// console.log(result2);
+
+// D-Task:
+
+// Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+const moment = require("moment");
+const time = moment().format("LLL");
+
+class Shop {
+  constructor(non, lagmon, cola) {
+    this.non = non;
+    this.lagmon = lagmon;
+    this.cola = cola;
+  }
+
+  qoldiq() {
+    console.log(
+      `Sizda ${time} da ${this.non} ta non, ${this.lagmon} ta lagmon va ${this.cola} ta cola qoldi`
+    );
+  }
+  sotish(non, lagmon, cola) {
+    this.non -= non;
+    this.lagmon -= lagmon;
+    this.cola -= cola;
+    console.log(
+      `Sizda ${time} da  ${non} ta non, ${lagmon} ta lagmon va ${cola} ta cola sotdingiz`
+    );
+  }
+  qabul(non, lagmon, cola) {
+    this.non += non;
+    this.lagmon += lagmon;
+    this.cola += cola;
+    console.log(
+      `Sizda ${time} da  ${non} ta non, ${lagmon} ta lagmon va ${cola} ta cola qabul qilib oldingiz`
+    );
+  }
+}
+
+const shop1 = new Shop(4, 5, 2);
+shop1.qoldiq();
+// shop1.qabul(1, 3, 4);
+shop1.sotish(3, 3, 2);
+shop1.qoldiq();
+
+console.log("------------");
