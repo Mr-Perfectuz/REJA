c3d94ce2bdd441eb1ed0e88340846a74be73082e
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2023-09-21 08:16:00 +0900

    EJS framework

--- server.js
+++ server.js
@@ -23,8 +23,14 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 Routing code
-app.get("/hello", function (req, res) {
-  res.end(`<h1 style="color:green">hello world</h1>`);
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success ! " });
+});
+
+app.get("/", function (req, res) {
+  res.render("harid");
+  // res.end(`<h1 style="color:green">hello world</h1>`);
 });
 app.get("/gift", function (req, res) {
   res.end(`<h1 style="color:green">You are in gifts section </h1>`);
@@ -36,3 +42,7 @@ let PORT = 3000;
 server.listen(PORT, function () {
   console.log`The server is running on port ${PORT}`;
 });
+
+// Start line  URL qismi
+//  HTTP request header
+//  Request body
